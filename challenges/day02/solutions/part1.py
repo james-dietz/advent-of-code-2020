ef3bb54ce787589cdf26b96b2bb80661448c627c
James Dietz <james-dietz@users.noreply.github.com>
2020-12-03 11:21:43 +0000

    [D2/P1] Also return password when parsing policy

--- challenges/day02/solutions/part1.py
+++ challenges/day02/solutions/part1.py
@@ -3,13 +3,13 @@ from typing import Tuple
 Policy = Tuple[str, int, int]
 
 
-def parse_policy(line: str) -> Policy:
+def parse_policy(line: str) -> Tuple[Policy, str]:
     """
     Parse a password policy, returning the required letter, and the minimum and maximum number of times it must occur.
     :param line: the line to parse
-    :return: a tuple containing the password policy
+    :return: a tuple containing the password policy, and the password
     """
-    policy, _ = line.split(sep=": ")
+    policy, password = line.split(sep=": ")
     character = policy[-1]
     min_occurrences, max_occurrences = policy.split(" ")[0].split("-")
-    return character, int(min_occurrences), int(max_occurrences)
+    return (character, int(min_occurrences), int(max_occurrences)), password
