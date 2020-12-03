f361da14ae91211cd3c390e501190ae328c39480
James Dietz <james-dietz@users.noreply.github.com>
2020-12-03 11:21:10 +0000

    [D2/P1] Rewrite parse test to also ensure password is returned

--- challenges/day02/tests/test_parse_policy.py
+++ challenges/day02/tests/test_parse_policy.py
@@ -5,15 +5,14 @@ from challenges.day02.solutions.part1 import parse_policy
 
 class TestParsePasswordPolicy(unittest.TestCase):
     inputs = [
-        ("1-3 a: abcde", ("a", 1, 3)),
-        ("1-3 b: cdefg", ("b", 1, 3)),
-        ("2-9 c: ccccccccc", ("c", 2, 9))
+        ("1-3 a: abcde", ("a", 1, 3), "abcde", True),
+        ("1-3 b: cdefg", ("b", 1, 3), "cdefg", False),
+        ("2-9 c: ccccccccc", ("c", 2, 9), "ccccccccc", True)
     ]
 
     def test_parse(self):
-        for line, expected_policy in self.inputs:
-            self.assertEqual(parse_policy(line), expected_policy)
-
+        for line, expected_policy, password, _ in self.inputs:
+            self.assertEqual(parse_policy(line), (expected_policy, password))
 
 if __name__ == '__main__':
     unittest.main()
