f800d5ab6ed03079fa0e487e265e7efe408b7b30
James Dietz <james-dietz@users.noreply.github.com>
2020-12-04 15:08:29 +0000

    [D4/P2] Define function signature for length rule check method, init

--- challenges/day04/solutions/part2.py
+++ challenges/day04/solutions/part2.py
@@ -1,2 +1,11 @@
 class LengthRule:
-    pass
+    def __init__(self, exact=4):
+        self.exact_length = exact
+
+    def check(self, input_str: str) -> bool:
+        """
+        Check if the provided string meets the criteria set when the rule was initialised.
+        :param input_str: The string to test
+        :return: whether the string meets the criteria
+        """
+        pass
