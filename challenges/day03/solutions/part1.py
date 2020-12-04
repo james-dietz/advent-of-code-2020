0d1cb8303d2264a531a18d094799abefe125b6f6
James Dietz <james-dietz@users.noreply.github.com>
2020-12-04 12:38:54 +0000

    [D3/P1] Implement forest walk algorithm
    Use modular arithmetic to simulate the forest structure wraparound in x-axis.

--- challenges/day03/solutions/part1.py
+++ challenges/day03/solutions/part1.py
@@ -1,5 +1,8 @@
 from typing import Tuple
 
+TREE = "#"
+BLANK = "."
+
 
 def trees_encountered(input: str, move: Tuple[int, int]) -> int:
     """
@@ -9,4 +12,16 @@ def trees_encountered(input: str, move: Tuple[int, int]) -> int:
     :param move:  the movement in cells [horizontal, vertical] for each step
     :return: The number of trees encountered.
     """
-    pass
+    forest = [[cell for cell in line] for line in input.split("\n")]
+    # specify a cell (x, y) in the forest using forest[y][x]
+    max_x, max_y = len(forest[0]), len(forest) - 1
+    x, y = (0, 0)
+    trees = 0
+    # walk until we reach the bottom row
+    while y < max_y - 1:
+        # forest wraps around in the x-axis
+        x = (x + move[0]) % max_x
+        y += move[1]
+        # count a tree if there is one in the cell (True == 1; False == 0)
+        trees += forest[y][x] == TREE
+    return trees
