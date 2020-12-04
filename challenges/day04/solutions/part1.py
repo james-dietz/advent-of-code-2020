922113067f3274d5b60c63327bfcf483ef605cf4
James Dietz <james-dietz@users.noreply.github.com>
2020-12-04 13:51:55 +0000

    [D4/P1] Define tests and function signatures for passport parsing
    Define the required and optional passport fields.
    Test for successful field parsing.
    Test passport validity by present fields.
    Add input file.

---
+++ challenges/day04/solutions/part1.py
@@ -0,0 +1,21 @@
+from typing import Set
+
+
+def extract_passport_fields(passport: str) -> Set[str]:
+    """
+    Extract the passport fields from a given passport string.
+    :param passport: The passport being processed
+    :return: A set containing all of the extracted fields.
+    """
+    pass
+
+
+def is_passport_valid(fields: Set[str]) -> bool:
+    """
+    Check if a passport is valid, under the following criteria:
+        - all fields specified by REQUIRED_FIELDS must be present for a passport to be valid
+        - fields specified by OPTIONAL_FIELDS may or may not be present
+    :param fields: a set containing the fields found in the passport
+    :return: whether the passport meets the above criteria
+    """
+    pass
---
+++ challenges/day04/tests/test_passport.py
@@ -0,0 +1,50 @@
+import unittest
+from typing import Tuple, Set, List
+
+from challenges.day04.solutions.part1 import extract_passport_fields, is_passport_valid
+
+REQUIRED_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+OPTIONAL_FIELDS = {"cid"}
+
+InputFormat = Tuple[str, Set[str], bool]
+
+
+class TestPassportParser(unittest.TestCase):
+    inputs: List[InputFormat] = [
+        # passport 1: valid, 8 fields present
+        (
+            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
+            {"ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt"},
+            True
+        ),
+        # passport 2: invalid, height ("hgt") missing
+        (
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
+            {"iyr", "ecl", "cid", "eyr", "pid", "hcl", "byr"},
+            False
+        ),
+        # passport 3: valid, cid missing but optional
+        (
+            "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
+            {"hcl", "iyr", "eyr", "ecl", "pid", "byr", "hgt"},
+            True
+        ),
+        # passport 4: invalid, byr missing
+        (
+            "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in",
+            {"hcl", "eyr", "pid", "iyr", "ecl", "hgt"},
+            False
+        )
+    ]
+
+    def test_parse_passport(self):
+        for passport, fields, _ in self.inputs:
+            self.assertEqual(extract_passport_fields(passport), fields)
+
+    def test_is_passport_valid(self):
+        for _, fields, is_valid in self.inputs:
+            self.assertEqual(is_passport_valid(fields), is_valid)
+
+
+if __name__ == '__main__':
+    unittest.main()
