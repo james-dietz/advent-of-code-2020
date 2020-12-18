1f58db9a77c449ce2b98039a0f57352660e7615e
James Dietz <james-dietz@users.noreply.github.com>
2020-12-18 23:43:54 +0000

    [D6/P1] Solve part 1 of day 6's problem
    Split the input file into groups of passengers by blank lines.
    Calculate the sum of the set of answered questions amongst the group.

--- challenges/day06/solutions/part1.py
+++ challenges/day06/solutions/part1.py
@@ -1,7 +1,7 @@
-from typing import Tuple, Set
+from typing import Set, List
 
 
-def group_questions_answered(questions: Tuple[str]) -> Set[str]:
+def group_questions_answered(questions: List[str]) -> Set[str]:
     """
     Return the set of questions for which at least one person answered correctly.
     :param questions: the questions each person answered correctly
@@ -18,3 +18,34 @@ def group_questions_answered(questions: Tuple[str]) -> Set[str]:
         for question in person:
             answered_questions.add(question)
     return answered_questions
+
+
+def split_into_groups(lines: List[str]) -> List[List[str]]:
+    """
+    Split the list of people's questions into groups, delimited by a blank line
+    :param lines: The list of lines
+    :return: A list of groups of passengers' questions
+    """
+    groups = []
+    current_group = []
+    for line in lines:
+        if line == "":
+            groups.append(current_group)
+            current_group = []
+        else:
+            current_group.append(line)
+    groups.append(current_group)
+    return groups
+
+
+def solve_part1(input_filename: str) -> int:
+    with open(input_filename, "r") as input_file:
+        groups = split_into_groups([line.rstrip("\n") for line in input_file.readlines()])
+        total = 0
+        for group in groups:
+            total += len(group_questions_answered(group))
+        return total
+
+
+if __name__ == '__main__':
+    print(solve_part1("../inputs/input.txt"))
