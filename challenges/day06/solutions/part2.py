2f0500aeffe7fbc1838308567ff9f5cebf5b46db
James Dietz <james-dietz@users.noreply.github.com>
2020-12-18 23:51:32 +0000

    [D6/P2] Add test cases, test and function signature for "all questions answered"

---
+++ challenges/day06/solutions/part2.py
@@ -0,0 +1,10 @@
+from typing import Tuple, Set
+
+
+def group_questions_all_answered(person_answered: Tuple[str]) -> Set[str]:
+    """
+    Find the set of questions for which all passengers answered yes.
+    :param person_answered: The questions each person in the group answered yes to.
+    :return: The set of questions for which all passengers answered yes
+    """
+    pass
--- challenges/day06/tests/test_group_questions.py
+++ challenges/day06/tests/test_group_questions.py
@@ -2,22 +2,27 @@ import unittest
 from typing import Tuple, Set, List
 
 from challenges.day06.solutions.part1 import group_questions_answered
+from challenges.day06.solutions.part2 import group_questions_all_answered
 
-TestCase = Tuple[Tuple[str], Set[str]]
+TestCase = Tuple[Tuple[str], Set[str], Set[str]]
 
 
 class TestGroupQuestions(unittest.TestCase):
     inputs: List[TestCase] = [
-        (("abc"), {"a", "b", "c"}),
-        (("a", "b", "c"), {"a", "b", "c"}),
-        (("ab", "ac"), {"a", "b", "c"}),
-        (("a", "a", "a", "a"), {"a"}),
-        (("b"), {"b"})
+        (("abc"), {"a", "b", "c"}, {"a", "b", "c"}),
+        (("a", "b", "c"), {"a", "b", "c"}, {}),
+        (("ab", "ac"), {"a", "b", "c"}, {"a"}),
+        (("a", "a", "a", "a"), {"a"}, {"a"}),
+        (("b"), {"b"}, {"b"})
     ]
 
     def test_group_questions_answered(self):
-        for person_answered, questions in self.inputs:
+        for person_answered, questions, _ in self.inputs:
             self.assertEqual(questions, group_questions_answered(person_answered))
+
+    def test_group_questions_all_answered(self):
+        for person_answered, _, questions in self.inputs:
+            self.assertEqual(questions, group_questions_all_answered(person_answered))
 
 
 if __name__ == '__main__':
