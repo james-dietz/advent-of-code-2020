843eef435344ce879f4e978c127eb86ebdcee053
James Dietz <james-dietz@users.noreply.github.com>
2020-12-18 23:28:43 +0000

    [D6/P1] Get the set of answered questions for each group

--- challenges/day06/solutions/part1.py
+++ challenges/day06/solutions/part1.py
@@ -8,3 +8,13 @@ def group_questions_answered(questions: Tuple[str]) -> Set[str]:
     :return: all questions answered correctly at least once
     """
     pass
+
+    # for each list of questions:
+    #   - take each question, add it to the set of answered questions
+    # return the set of answered questions
+
+    answered_questions = set()
+    for person in questions:
+        for question in person:
+            answered_questions.add(question)
+    return answered_questions
