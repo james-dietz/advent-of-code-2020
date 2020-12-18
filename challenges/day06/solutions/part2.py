53781891e44b5d8a23f93efae48a629513c5e2ad
James Dietz <james-dietz@users.noreply.github.com>
2020-12-18 23:59:09 +0000

    [D6/P2] Get the set of questions for which all group members answered yes

--- challenges/day06/solutions/part2.py
+++ challenges/day06/solutions/part2.py
@@ -7,4 +7,12 @@ def group_questions_all_answered(person_answered: Tuple[str]) -> Set[str]:
     :param person_answered: The questions each person in the group answered yes to.
     :return: The set of questions for which all passengers answered yes
     """
-    pass
+    if len(person_answered) == 1:
+        return set(question for question in person_answered[0])
+    else:
+        questions_answered = set(question for question in person_answered[0])
+        for questions in person_answered[1:]:
+            question_set = set(question for question in questions)
+            questions_answered.intersection_update(question_set)
+
+    return questions_answered
